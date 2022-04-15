38ad34cb30437a3f3e0eaf047456f20d691a0da0
morlakdzuu <xerandom@mail.ru>
2022-04-15 21:30:26 +0300

    init

--- ChatModule/scripts/deploy.sh
+++ ChatModule/scripts/deploy.sh
@@ -2,21 +2,7 @@
 
 mvn clean package
 
-echo 'Copy files >>>>'
-
-scp -i /Users/morlakDzuu/.ssh/id_rsa \
-    target/demo-0.0.1-SNAPSHOT.jar \
-    morlak@178.154.210.140:/home/morlak/spring
-
-echo 'Restart server >>>>>'
-
-ssh -i /Users/morlakDzuu/.ssh/id_rsa morlak@178.154.210.140 << EOF
-
 pgrep java | xargs kill -9
-cd spring
-rm log.txt
-nohup java -jar demo-0.0.1-SNAPSHOT.jar > log.txt &
-
-EOF
+nohup java -jar target/demo-0.0.1-SNAPSHOT.jar  > log.txt &
 
 echo 'Goodbye))'
